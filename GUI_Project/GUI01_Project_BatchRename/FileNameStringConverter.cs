fea0a2dfa196e3d7b58c57cd4c5c886b341ab6d5
Mjnk13 <92595891+Mjnk13@users.noreply.github.com>
2021-11-29 22:58:06 +0700

    Add files via upload

---
+++ GUI_Project/GUI01_Project_BatchRename/FileNameStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GUI01_Project_BatchRename
+{
+    class FileNameStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string _FilePath = (string)value;
+            return Path.GetFileName(_FilePath);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
